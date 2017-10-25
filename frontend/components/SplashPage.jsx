226e1b52b17eb5dd9eee6d593344b385b9297f6c
Jonathan Lin <jclin2013@gmail.com>
2017-10-25 14:42:49 -0400

    Build out dropdown menu and submit button

--- frontend/components/SplashPage.jsx
+++ frontend/components/SplashPage.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+// import DiseasesDropdown from './DiseasesDropdown';
 import { Link } from 'react-router-dom';
 import { meshDiseaseCategories } from './meshDiseaseCategories';
 
 class SplashPage extends React.Component {
   constructor() {
     super()
+    this.state = { diseaseSelected: "Cardiovascular Diseases" };
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler(e) {
+    this.setState({diseaseSelected: e.currentTarget.value});
   }
 
   render() {
+
     return (
-      <div>
-        <div>Welcome to splash page!</div>
-        <Link to={`/lookup/Cardiovascular Diseases`}>Cardiovascular Diseases</Link>
+      <div id='SplashPage'>
+        <div>
+          <h1>DiseaseLookup</h1>
+        </div>
+
+        <select onChange={this.changeHandler} size='5' id="diseasesDropdown">
+          {meshDiseaseCategories.map(disease =>
+            <option key={disease}>{disease}</option>
+          )}
+        </select>
+
+        <Link to={`/lookup/${this.state.diseaseSelected}`}>Submit</Link>
       </div>
     )
   }
