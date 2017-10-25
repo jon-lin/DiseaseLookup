9583f84f08d8bd32c736b47a0586dc8425b568e9
Jonathan Lin <jclin2013@gmail.com>
2017-10-25 12:35:30 -0400

    Begin work on frontend, add webpack config, react-viewport

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/index.jsx',
+  output: {
+    filename: './frontend/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
